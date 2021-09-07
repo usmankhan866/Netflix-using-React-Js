9b7c33d3913bb9955479c7ca5d7733988bf7d780
Mohammad Usman <usmankhann866@gmail.com>
2021-09-07 16:14:23 +0500

    Cards data are moved to a new component called :CardData:

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
-import React from "react"
+import React from "react";
 import { Cards } from "./Cards";
+import CardData from "./CardData";
 
 
 function App(){
 
   return(
     <>     
-    <Cards imgSrc="http://wallpapercave.com/wp/wp4056410.jpg" 
-           title="A Netflix Orignal Series"
-           sname="Money Heist"
-           link="" />,
-    
-    <Cards imgSrc="http://wallpapercave.com/wp/wp4056410.jpg" 
-           title="A Netflix Orignal Series"
-           sname="DARK"
-           link="" />,
+    <Cards 
+       imgSrc ={CardData[0].imgSrc} 
+       title = {CardData[0].title }
+       sname = {CardData[0].sname}
+       link  = {CardData[0].link}
+   />
 
-    <Cards imgSrc="http://wallpapercave.com/wp/wp4056410.jpg" 
-           title="A Netflix Orignal Series"
-           sname="DARK"
-           link="" />       
-           
-    <Cards imgSrc="http://wallpapercave.com/wp/wp4056410.jpg" 
-           title="A Netflix Orignal Series"
-           sname="DARK"
-           link="" />
+   <Cards 
+       imgSrc ={CardData[1].imgSrc} 
+       title = {CardData[1].title }
+       sname = {CardData[1].sname}
+       link  = {CardData[1].link}
+   />
+                      
+   <Cards 
+       imgSrc ={CardData[2].imgSrc} 
+       title = {CardData[2].title }
+       sname = {CardData[2].sname}
+       link  = {CardData[2].link}
+   />
 
-     <Cards imgSrc="http://wallpapercave.com/wp/wp4056410.jpg" 
-           title="A Netflix Orignal Series"
-           sname="DARK"
-           link="" />       
+   <Cards 
+       imgSrc ={CardData[3].imgSrc} 
+       title = {CardData[3].title }
+       sname = {CardData[3].sname}
+       link  = {CardData[3].link}
+   />
+            
+   <Cards 
+       imgSrc ={CardData[4].imgSrc} 
+       title = {CardData[4].title }
+       sname = {CardData[4].sname}
+       link  = {CardData[4].link}
+   />
 
-      <Cards imgSrc="http://wallpapercave.com/wp/wp4056410.jpg" 
-           title="A Netflix Orignal Series"
-           sname="DARK"
-           link="" />       
-             
-      
+   <Cards 
+       imgSrc ={CardData[5].imgSrc} 
+       title = {CardData[5].title }
+       sname = {CardData[5].sname}
+       link  = {CardData[5].link}
+   />
+                              
+         
   </>
   );
   }
---
+++ src/CardData.js
@@ -0,0 +1,51 @@
+import react from "react";
+
+
+
+let CardData = [
+
+    {
+        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
+           title:"A Netflix Orignal Series",
+           sname:"Money Heist",
+           link:""
+    },
+
+    {
+        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
+           title:"A Netflix Orignal Series",
+           sname:"DARK",
+           link:""
+
+    },
+
+    {
+        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
+           title:"A Netflix Orignal Series",
+           sname:"DARK",
+           link:""
+    },
+
+    {
+        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
+           title:"A Netflix Orignal Series",
+           sname:"DARK",
+           link:""
+    },
+
+    {
+        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
+           title:"A Netflix Orignal Series",
+           sname:"DARK",
+           link:""
+    },
+
+    {
+        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
+           title:"A Netflix Orignal Series",
+           sname:"DARK",
+           link:""
+    }
+];
+
+export default CardData;
