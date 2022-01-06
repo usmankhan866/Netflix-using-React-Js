c0479a67f3727797f52569f7dab158cfc3e3895b
Mohammad Usman <usmankhann866@gmail.com>
2022-01-06 16:12:36 +0500

    App Updated

--- src/CardData.js
+++ src/CardData.js
@@ -6,51 +6,51 @@ let CardData = [
 
     {
         id: 1,
-        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
-           title:"A Netflix Orignal Series",
-           sname:"Money Heist",
-           link:""
+        imgSrc:"https://m.media-amazon.com/images/M/MV5BNDJkYzY3MzMtMGFhYi00MmQ4LWJkNTgtZGNiZWZmMTMxNzdlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg", 
+        title:"A Netflix Orignal Series",
+        sname:"Money Heist",
+        link:"https://www.netflix.com/search?q=money%20heist"
     },
 
     {
         id: 2,
-        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
-           title:"A Netflix Orignal Series",
-           sname:"DARK",
-           link:""
+        imgSrc:"https://theculturednerd.org/wp-content/uploads/2020/05/the-dark-poster.jpg", 
+        title:"A Netflix Orignal Series",
+        sname:"DARK",
+        link:"https://www.netflix.com/search?q=dark"
 
     },
 
     {
         id: 3,
-        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
-           title:"A Netflix Orignal Series",
-           sname:"DARK",
-           link:""
+        imgSrc:"https://m.media-amazon.com/images/M/MV5BZmRjODgyMzEtMzIxYS00OWY2LTk4YjUtMGMzZjMzMTZiN2Q0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg", 
+        title:"A Netflix Orignal Series",
+        sname:"Red Notice",
+        link:"https://www.netflix.com/search?q=red%20notice"
     },
 
     {
         id: 4,
-        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
-           title:"A Netflix Orignal Series",
-           sname:"DARK",
-           link:""
+        imgSrc:"https://m.media-amazon.com/images/M/MV5BMmEyM2JkN2ItNmQ3NS00ZTkzLWEwMjYtZmU1YmJlODczZWM1XkEyXkFqcGdeQWFybm8@._V1_.jpg", 
+        title:"A Netflix Orignal Series",
+        sname:"Monster Hunter",
+        link:"https://www.netflix.com/search?q=monster%20hunter"
     },
 
     {
         id: 5,
-        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
-           title:"A Netflix Orignal Series",
-           sname:"DARK",
-           link:""
+        imgSrc:"https://moderndiplomacy.eu/wp-content/uploads/2021/10/squid-game-rules.jpg", 
+        title:"A Netflix Orignal Series",
+        sname:"Squid Game",
+        link:"https://www.netflix.com/search?q=squid%20game"
     },
 
     {
         id: 6,
-        imgSrc:"http://wallpapercave.com/wp/wp4056410.jpg", 
-           title:"A Netflix Orignal Series",
-           sname:"DARK",
-           link:""
+        imgSrc:"https://www.cnet.com/a/img/to4uGveXvDk35-9UQWzCRBq_M1w=/940x0/2017/06/22/3c34448b-c996-4058-b3ec-00973ffa95af/supergirl-11.jpg", 
+        title:"A Netflix Orignal Series",
+        sname:"Super Girl",
+        link:"https://www.netflix.com/search?q=Super%20Girl"
     }
 ];
 
